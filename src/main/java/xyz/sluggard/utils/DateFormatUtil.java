7e5ae4d20f069f924384af826e7040f5f7b94360
frank <sydra@163.com>
2017-12-15 10:55:14 +0800

    first commit

---
+++ src/main/java/xyz/sluggard/utils/DateFormatUtil.java
@@ -0,0 +1,64 @@
+package xyz.sluggard.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormatUtil {
+
+    private static final String DATETIME_PATTERN = "yyyy-MM-dd hh:mm:ss";
+
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
+
+    private static final String TIME_PATTERN = "hh:mm:ss";
+
+    private static ThreadLocal<SimpleDateFormat> dataTimeFormat = new ThreadLocal(){
+
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat(DATETIME_PATTERN);
+        }
+    };
+
+    private static ThreadLocal<SimpleDateFormat> dataFormat = new ThreadLocal(){
+
+        @Override
+        public SimpleDateFormat initialValue() {
+            return new SimpleDateFormat(DATE_PATTERN);
+        }
+    };
+
+    private static ThreadLocal<SimpleDateFormat> timeFormat = new ThreadLocal(){
+
+        @Override
+        public SimpleDateFormat initialValue() {
+            return new SimpleDateFormat(TIME_PATTERN);
+        }
+    };
+
+    public static Date dateTimeParse(String source) throws ParseException {
+        return dataTimeFormat.get().parse(source);
+    }
+
+    public static String dateTimeFormat(Date date) {
+        return dataTimeFormat.get().format(date);
+    }
+
+    public static Date dateParse(String source) throws ParseException {
+        return dataFormat.get().parse(source);
+    }
+
+    public static String dateFormat(Date date) {
+        return dataFormat.get().format(date);
+    }
+
+    public static Date timeParse(String source) throws ParseException {
+        return timeFormat.get().parse(source);
+    }
+
+    public static String timeFormat(Date date) {
+        return timeFormat.get().format(date);
+    }
+
+
+}
---
+++ src/main/java/xyz/sluggard/utils/StringUtil.java
@@ -0,0 +1,15 @@
+package xyz.sluggard.utils;
+
+import java.util.UUID;
+
+public abstract class StringUtil {
+
+    private static final char[] number = {0,1,2,3,4,5,6,7,8,9};
+
+    private static final char[] alphabetLow = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    private static final char[] alphabetUP = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+
+
+}
